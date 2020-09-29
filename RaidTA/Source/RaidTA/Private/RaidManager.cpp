01d7ed1f1be11ca654ff9f8b8d9919d84e3e77c5
Austin <jacoba4@rpi.edu>
2020-09-29 17:49:53 -0400

    Added test_sprite, Unit.cpp/.h, and RaidManager.cpp/.h

---
+++ RaidTA/Source/RaidTA/Private/RaidManager.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RaidManager.h"
+
+RaidManager::RaidManager()
+{
+}
+
+RaidManager::~RaidManager()
+{
+}
---
+++ RaidTA/Source/RaidTA/Public/Unit.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class RAIDTA_API Unit
+{
+public:
+	Unit();
+	~Unit();
+};

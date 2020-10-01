b7c1eb326ff3d25f70f6abaa7cd784ed4e5388c6
Brian Mejia <bmejia0727@gmail.com>
2020-10-01 16:20:46 -0400

    new gitignore applied

--- RaidTA/Source/RaidTA/Public/Unit.h
+++ RaidTA/Source/RaidTA/Public/Unit.h
@@ -3,13 +3,39 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Unit.generated.h"
 
-/**
- * 
- */
-class RAIDTA_API Unit
+UCLASS()
+class RAIDTA_API AUnit : public APawn
 {
+	GENERATED_BODY()
+
 public:
-	Unit();
-	~Unit();
+	// Sets default values for this pawn's properties
+	AUnit();
+	~AUnit();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Called to teleport a Unit to a location
+	void MoveToLocation(FVector MoveLocation);
+
+private:
+	int max_hp;
+	int hp;
+	int damage;
+	bool is_healer;
+	bool has_command; // Move this to Player Unit class when made?
+	FVector target_destination;
+	AUnit* current_target;
 };

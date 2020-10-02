39b382def34a1fb778b16ae747fff3dec1427e72
Austin <jacoba4@rpi.edu>
2020-10-01 21:51:26 -0400

    Started work on RaidManager
    Unit selection and calling movement works now

--- RaidTA/Source/RaidTA/Private/RaidManager.cpp
+++ RaidTA/Source/RaidTA/Private/RaidManager.cpp
@@ -3,10 +3,79 @@
 
 #include "RaidManager.h"
 
-RaidManager::RaidManager()
+// Sets default values
+ARaidManager::ARaidManager()
 {
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+	
 }
 
-RaidManager::~RaidManager()
+ARaidManager::~ARaidManager()
 {
+
 }
+
+// Called when the game starts or when spawned
+void ARaidManager::BeginPlay()
+{
+	Super::BeginPlay();
+		
+
+	UE_LOG(LogTemp, Warning, TEXT("Init"));
+	raid_size = raid_array.Num();
+	selected_units.Init(false, raid_size);
+}
+
+// Called every frame
+void ARaidManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ARaidManager::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Input"));
+	//Super::SetupPlayerInputComponent(InputComponent);
+	InputComponent->BindAction("Select_Unit_0", IE_Pressed, this, &ARaidManager::SelectUnit0);
+	InputComponent->BindAction("Select_Unit_1", IE_Pressed, this, &ARaidManager::SelectUnit1);
+	InputComponent->BindAction("Click", IE_Pressed, this, &ARaidManager::SendMouseCoordinate);
+}
+void ARaidManager::SelectUnit0()
+{
+	UE_LOG(LogTemp, Warning, TEXT("SelectUnit0"));
+	ClearSelection();
+	selected_units[0] = true;
+}
+
+void ARaidManager::SelectUnit1()
+{
+	ClearSelection();
+	selected_units[1] = true;
+}
+
+void ARaidManager::ClearSelection()
+{
+	for (int i = 0; i < selected_units.Num(); i++)
+	{
+		selected_units[i] = false;
+	}
+}
+
+void ARaidManager::SendMouseCoordinate()
+{
+	APlayerController* playerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	FHitResult hit;
+	playerController->GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, false, hit);
+	
+	for (int i = 0; i < raid_size; i++)
+	{
+		if (selected_units[i])
+		{
+			raid_array[i]->MoveToLocation(hit.Location);
+		}
+	}
+}
+
--- RaidTA/Source/RaidTA/Public/RaidManager.h
+++ RaidTA/Source/RaidTA/Public/RaidManager.h
@@ -2,14 +2,42 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "Unit.h"
+#include "Components/InputComponent.h"
+#include "Kismet/GameplayStatics.h"
 
-/**
- * 
- */
-class RAIDTA_API RaidManager
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "RaidManager.generated.h"
+
+
+UCLASS()
+class RAIDTA_API ARaidManager : public APawn
 {
-public:
-	RaidManager();
-	~RaidManager();
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ARaidManager();
+	~ARaidManager();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent);
+	void SelectUnit0();
+	void SelectUnit1();
+	void ClearSelection();
+	void SendMouseCoordinate();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Raid")
+	int raid_size;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Raid")
+	TArray<AUnit*> raid_array;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Raid")
+	TArray<bool> selected_units;
 };
